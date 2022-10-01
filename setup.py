30cefd07a047e72d41cf0fc62ba6ac86c4dc184d
Vincent Lin <soulace2425@outlook.com>
2022-10-01 01:37:24 -0700

    Move config files to root directory and edit them

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""setup.py
+
+Package setup script.
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
--- src/setup.py
+++
@@ -1,9 +0,0 @@
-"""
-setup.py
-23 July 2022 00:06:47
-"""
-
-from setuptools import setup
-
-# Configuration set up in setup.cfg
-setup()
