ece3792a9aab95940367a61756adb6f582fdd811
Vincent Lin <soulace2425@outlook.com>
2022-10-01 15:39:53 -0700

    Fix install.py

--- scripts/install.py
+++ scripts/install.py
@@ -5,19 +5,22 @@ distribution in my global environment.
 """
 
 from pathlib import Path
-import os
 import sys
 import subprocess
 
 CONFIG_FILE_PATH = Path.home() / ".config/waifu-roller/config.yaml"
 
-
-# Redefine print for minimal log-like outputs
 _print = print
 
 
 def print(*args, **kwargs) -> None:
-    return _print(f"[{__file__}]", *args, **kwargs)
+    """Redefine print for minimal log-like outputs."""
+    return _print(f"[{Path(__file__).name}]", *args, **kwargs)
+
+
+def get_python_path() -> Path:
+    """Return resolved path to the global interpreter, probably."""
+    return (Path(sys.base_prefix) / "python.exe").resolve()
 
 
 def get_latest_dist() -> Path | None:
@@ -27,22 +30,24 @@ def get_latest_dist() -> Path | None:
     for path in dist_path.iterdir():
         pass
     if path is not None:
+        path = path.resolve()
         print(f"Using distribution {path}.")
     else:
         print("Could not find a whl file to install.")
     return path
 
 
-def install_dist(whl: Path) -> None:
-    pip_cmd = "pip3" if os.name == "posix" else "pip"
-    subprocess.run(f"{pip_cmd} install {whl}", check=True)
-    print(f"Installed {whl}.")
+def install_dist_globally(whl: Path) -> None:
+    python_path = get_python_path()
+    cmd = f"{python_path} -m pip install {whl} --force-reinstall"
+    subprocess.run(cmd, check=True)
+    print(f"Installed {whl} with Python interpreter at {python_path}.")
 
 
 def remove_config_file() -> None:
     try:
         CONFIG_FILE_PATH.unlink()
-        print(f"Delete config file at {CONFIG_FILE_PATH}.")
+        print(f"Deleted config file at {CONFIG_FILE_PATH}.")
     except FileNotFoundError:
         print(
             f"Could not find config file at {CONFIG_FILE_PATH}, "
@@ -52,18 +57,22 @@ def remove_config_file() -> None:
 
 def main() -> None:
     """Main driver function."""
-    # Install the latest distribution, if it exists
+    # Get the latest distribution, if it exists
     dist_path = get_latest_dist()
     if dist_path is None:
         sys.exit(1)
-    install_dist(dist_path)
+
+    # Install with global Python, probably
+    install_dist_globally(dist_path)
 
     # If successful, remove user's existing config file for fresh start
     remove_config_file()
 
-    # Verify that the package is installed
-    print("Verifying installation by running: waifu --version")
-    subprocess.run("waifu --version", check=True)
+    # Tell user to verify that the package is installed
+    print(
+        "Verify that the package is installed globally by running 'waifu "
+        "--version' outside of your virtual environment."
+    )
 
 
 if __name__ == "__main__":
